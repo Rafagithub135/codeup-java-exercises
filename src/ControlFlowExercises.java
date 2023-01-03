bb7cbfd8202727eed3d7c28bef179f577bd79375
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-03 11:23:43 -0500

    completed second exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -29,5 +29,17 @@ public class ControlFlowExercises {
 //for (long i = 2; i < 1000000; i *= i) {
 //    System.out.println(i);
 //}
+// Fizzbuzz
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 15 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
     }
 }
