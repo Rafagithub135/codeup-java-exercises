423c5aaf3a2023cd2825aeb25675f62dcc1018e0
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-04 13:08:39 -0500

    created methodsexercises

---
+++ src/MethodsExercises.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class MethodsExercises {
+    public static int main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // 1. Basic Arithmetic
+        // Addition
+        int num1, num2;
+        System.out.println("Enter first number to add: ");
+        num1 = scanner.nextInt();
+        System.out.println("Enter second number to add: ");
+        num2 = scanner.nextInt();
+//        public static int addition ( int num1, int num2){
+            return num1 + num2;
+        }
+//        public static int subtraction ( int num1, int num2){
+//            return num1 - num2;
+        }
+//        public static int multiplication ( int num1, int num2){
+//            return num1 * num2;
+//        }
+        //
+        // Code from javatpoint.com/multiply-two-numbers-without-using-operator-in-java, modified slightly.
+//        int x, y, sum = 0;
+//        System.out.print("Enter first number to multiply: ");
+//        x = scanner.nextint();
+//        System.out.print("Enter second number to multiply: ");
+//        y = scanner.nextint();
+//        for (int i = 1; i <= x; i++) {
+//            sum += y;
+//        }
+//        System.out.println("The product of " + x + " and " + y + " is " + sum);
+
+//        public static int division ( int num1, int num2){
+//            return num1 / num2;
+//        }
+//        public static int modulus ( int num1, int num2){
+//            return num1 % num2;
+//        }
+
+//    }
+//}
+
