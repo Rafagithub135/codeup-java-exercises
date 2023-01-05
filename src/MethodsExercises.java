f8ec778c08002b095435e39e5723a803b2144986
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-05 15:18:48 -0500

    need assistance, transferring to other computer

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -38,23 +38,33 @@ public class MethodsExercises {
             return getInteger(min, max);
         }
     }
+    public static int factorial(int num){
+        System.out.println("Enter an integer between 1 and 10: ");
+        int userInput = scanner.nextInt();
+        for (int i = 1; i <= num; i++) {
+            System.out.println(num + "! )
+        }
+        return ;
+    }
 
 
     public static void main(String[] args) {
 
         // 1. Basic Arithmetic
         // Addition
-        System.out.println(addition(5, 7));
-
-        System.out.println(subtraction(10, 5));
-
-        System.out.println(multiplication(5, 5));
-
-        System.out.println(division(10, 3));
-        // If you try to divide by 0, the code breaks.  Since you can't devide a number by zero I would think the one line should break.
-
-        System.out.println(modulus(10, 3));
-
-        getInteger(1, 732);
+//        System.out.println(addition(5, 7));
+        // Subtraction
+//        System.out.println(subtraction(10, 5));
+        // Multiplication
+//        System.out.println(multiplication(5, 5));
+        // Division
+//        System.out.println(division(10, 3));
+        // If you try to divide by 0, the code breaks.  Since you can't divide a number by zero I would think the one line should break.
+        // Modulus
+//        System.out.println(modulus(10, 3));
+        // 2. Get Integer
+//        getInteger(1, 732);
+        // 3. Factorial
+        factorial(5);
     }
 }
