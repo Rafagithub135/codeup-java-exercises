8d5c28069b88b1e056edf24b19e57bd3ed3f03c0
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-06 12:24:41 -0500

    completed methods exercises

--- src/HighLow.java
+++ src/HighLow.java
@@ -13,19 +13,23 @@ public class HighLow {
 
     public static void guessTheNumber() {
         int specNumber = (int) (Math.random() * 100);
-        System.out.println("I'm thinking of a number between 1 and 100.  Can you guess it?");
+        System.out.println("I'm thinking of a number between 1 and 100.  Can you guess it? You get 10 guesses.");
         boolean isCorrect = true;
+        int count = 0;
         do {
+            count++;
+            if (count > 10) {
+                return;
+            }
+            System.out.println(count);
             int number = scanner.nextInt();
             if (number == specNumber) {
                 System.out.println("GOOD GUESS!");
-                isCorrect = false;
+                return;
             } else if (number < specNumber) {
                 System.out.println("HIGHER");
-//                guessAgain();
             } else if (number > specNumber) {
                 System.out.println("LOWER");
-//                    guessAgain();
             } else {
                 System.out.println("Invalid input. Continue? [y/n]");
                 String userInput = scanner.next();
@@ -37,13 +41,6 @@ public class HighLow {
     }
 
     public static void main(String[] args) {
-
-
         guessTheNumber();
-
-
-//
-
-
     }
 }
