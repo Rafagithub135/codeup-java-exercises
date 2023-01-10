c437660371a09e1e04146bcc9734fecbc1473692
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-10 12:29:49 -0500

    completed ConsoleExercises

---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,12 @@
+import util.Input;
+
+//package shapes;
+//
+//public class CircleApp {
+//
+//        public static void main(String[] args) {
+//            Circle circle = new Circle(5);
+//            System.out.println("The area of a circle of radius " + circle.getRadius() + " is: " + circle.getArea());
+//            System.out.println("The circumference of a circle of radius " + circle.getRadius() + " is: " + circle.getCircumference());
+//        }
+//}
