b6ec733443fc71aa485c32a2e12e3d649e54dcbb
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-10 15:37:11 -0500

    completed OOP exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,30 +1,65 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
-    private class scanner{
-        public static String getString(String prompt){
-            return prompt;
-        }
-        boolean yesNo(boolean prompt){
-            return true;
-        }
-        int getInt(int min, int max){
-            return min;
-        }
-        int getInt(){
-            return 0;
-        }
-        double getDouble(double min, double max){
-            return 0;
-        }
-        double getDouble(){
-            return 0;
-        }
+    private Scanner scanner;
 
-        public class InputTest {
-            public static void main(String[] args) {
-
-            }
-        }
+    public Input() {
+        scanner = new Scanner(System.in);
     }
+
+//    public String getString() {
+//        System.out.println("Enter a string: ");
+//        return scanner.nextLine();
+//    }
+
+//    public String getString(String prompt) {
+//        System.out.println(prompt);
+//        return scanner.nextLine();
+//    }
+
+//    public boolean yesNo() {
+//        String userString = getString("Please enter yes or no: ");
+//        if (userString.equalsIgnoreCase("y") || userString.equalsIgnoreCase("yes")) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+
+//    public int getInt(int min, int max) {
+//        System.out.printf("Enter a number between %d and %d: ", min, max);
+//        int userInput = scanner.nextInt();
+//        if (userInput < min || userInput > max) {
+//            System.out.println("Invalid input.");
+//            return getInt(min, max);
+//        } else {
+//            return userInput;
+//        }
+//    }
+
+//    public int getInt() {
+//        System.out.println("Enter an integer: ");
+//        int userInput = scanner.nextInt();
+//        return userInput;
+//    }
+
+//    public double getDouble(double min, double max) {
+//        System.out.printf("Enter a number between %f and %f: ", min, max);
+//        double userInput = scanner.nextDouble();
+//        if (userInput < min || userInput > max) {
+//            System.out.println("Invalid input.");
+//            return getDouble(min, max);
+//        } else {
+//            return userInput;
+//        }
+//    }
+
+//    public double getDouble() {
+//        System.out.println("Enter a decimal number: ");
+//        double userInput = scanner.nextDouble();
+//        return userInput;
+//    }
+
 }
