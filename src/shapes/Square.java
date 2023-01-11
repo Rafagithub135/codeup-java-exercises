2a819b76a7136c0fadcf43c2189b4e198e2b6d34
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-11 09:02:18 -0500

    completed inheritance and polymorphism exercise

---
+++ src/shapes/Square.java
@@ -0,0 +1,21 @@
+package shapes;
+
+public class Square extends Rectangle {
+
+    public Square(int side) {
+        super(side, side);
+    }
+
+    public int getArea() {
+        return (int) Math.pow(side, 2);
+    }
+
+    public int getPerimeter() {
+        return 4 * side;
+    }
+
+    public static void main(String[] args) {
+        Square square1 = new Square(5);
+        System.out.printf("The area of the square is %d.%nThe perimeter of the square is %d.", square1.getArea(), square1.getPerimeter());
+    }
+}
