ff2fdaa11c2d1646fe642b30d95610b1769583c4
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-11 16:44:43 -0500

    completed most of movie exercise

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -12,7 +12,58 @@ public class MoviesApplication {
         System.out.println("3 - view movies in the drama category");
         System.out.println("4 - view movies in the horror category");
         System.out.println("5 - view movies in the scifi category");
-        System.out.println("6 - add a movie to the list");
-        userInput = scanner.nextLine();
+        System.out.println("6 - view movies in the comedy category");
+        System.out.println("7 - view movies in the musical category");
+        System.out.println("8 - add a movie to the list");
+        int userInput = scanner.nextInt();
+        if (userInput == 0) {
+            System.out.println("Goodbye!");
+        } else if (userInput == 1) {
+            for (Movie movie : MoviesArray.findAll()) {
+                System.out.println(movie.getName() + " -- " + movie.getCategory());
+            }
+        } else if (userInput == 2) {
+            for (Movie movie : MoviesArray.findAll()) {
+                if (movie.getCategory().equals("animated")) {
+                    System.out.println(movie.getName() + " -- " + movie.getCategory());
+                }
+            }
+        } else if (userInput == 3) {
+            for (Movie movie : MoviesArray.findAll()) {
+                if (movie.getCategory().equals("drama")) {
+                    System.out.println(movie.getName() + " -- " + movie.getCategory());
+                }
+            }
+        } else if (userInput == 4) {
+            for (Movie movie : MoviesArray.findAll()) {
+                if (movie.getCategory().equals("horror")) {
+                    System.out.println(movie.getName() + " -- " + movie.getCategory());
+                }
+            }
+        } else if (userInput == 5) {
+            for (Movie movie : MoviesArray.findAll()) {
+                if (movie.getCategory().equals("scifi")) {
+                    System.out.println(movie.getName() + " -- " + movie.getCategory());
+                }
+            }
+        } else if (userInput == 6) {
+            for (Movie movie : MoviesArray.findAll()) {
+                if (movie.getCategory().equals("comedy")) {
+                    System.out.println(movie.getName() + " -- " + movie.getCategory());
+                }
+            }
+        } else if (userInput == 7) {
+            for (Movie movie : MoviesArray.findAll()) {
+                if (movie.getCategory().equals("musical")) {
+                    System.out.println(movie.getName() + " -- " + movie.getCategory());
+                }
+            }
+        } else if (userInput == 8) {
+            System.out.println("Enter the name of the movie you would like to add:");
+            String movieName = scanner.next();
+            System.out.println("Enter the category of the movie you would like to add:");
+            String movieCategory = scanner.next();
+            Movie newMovie = new Movie(movieName, movieCategory);
+        }
     }
 }
