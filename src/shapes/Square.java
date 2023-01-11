a866a92f129e6f3e6f54dc53777ee62b089d29b2
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-11 12:37:28 -0500

    completed interfaces and abstract classes exercise

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,6 +1,34 @@
 package shapes;
 
-public class Square extends Rectangle {
+public class Square extends Quadrilateral {
+
+
+
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    @Override
+    public void setLength(double length) {
+
+    }
+
+    @Override
+    public void setWidth(double width) {
+
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * length;
+    }
+
+    @Override
+    public double getArea() {
+        return (int) Math.pow(length, 2);
+    }
+}
 
 //    public Square(int side) {
 //        super(side, side);
@@ -18,4 +46,3 @@ public class Square extends Rectangle {
 //        Square square1 = new Square(5);
 //        System.out.printf("The area of the square is %d.%nThe perimeter of the square is %d.", square1.getArea(), square1.getPerimeter());
 //    }
-}
